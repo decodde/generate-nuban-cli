270dd4f8b75b113a70a7201f50005bc5e035bced
Fasina Oreoluwa <dannyoma75@gmail.com>
2022-07-27 12:07:41 +0100

    +*[feat] added outputDirectory for saving result

--- bin/commands.js
+++ bin/commands.js
@@ -1,46 +1,68 @@
 const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
 
 const conf = new (require('conf'))()
 let defaults = {
-    limit : 100000,
-    startFrom : '000000000',
+    limit: 100000,
+    startFrom: '000000000',
+    outputDir : (bankCode) =>  path.join(process.cwd(),`${bankCode}_${Date.now()}.json`)
 }
 
-const {nubanAlgo} = require("./nubanAlgo");
+const { nubanAlgo } = require("./nubanAlgo");
 const Commands = {
-    generate : async (data) => {
+    generate: async (data) => {
         console.log(data);
-        let {bankCode,limit,random,startFrom} = await data;
-        if(bankCode > 0){
+        let { bankCode, limit, save, random, startFrom, print, outputDir } = await data;
+        if (bankCode > 0) {
             limit > 0 ? limit : limit = defaults.limit;
-            
+
             startFrom > 0 ? "" : startFrom = defaults.startFrom;
             console.log(startFrom);
-            if (startFrom.length > 9){
+            if (startFrom.length > 9) {
                 console.log(chalk.red.bold("Invalid NUBAN startFrom. Should not be greater than 9 digits"))
                 return;
             }
             console.log(chalk.yellow.bold(`Cooking up ${limit} NUBAN list for bank ${bankCode}....`));
 
-            let nubans = await nubanAlgo.generate_nuban_starting_from(startFrom,'up',limit,bankCode);
+            let nubans = await nubanAlgo.generate_nuban_starting_from(startFrom, 'up', limit, bankCode);
+            let beautifiedResult = [];
+        
             nubans.forEach((nuban, n) => {
-                console.log(chalk.green(n+"."), chalk.yellowBright(nuban));
+                beautifiedResult.push({
+                    sn : n,
+                    nuban : nuban
+                })
+                if(print) console.log(chalk.green(n + "."), chalk.yellowBright(nuban));
             })
+            if(save){
+                console.log(outputDir);
+                outputDir ? "" : outputDir = defaults.outputDir(bankCode);
+                
+                beautifiedResult = JSON.stringify(beautifiedResult, null ,'\t');
+
+                fs.writeFile(outputDir,beautifiedResult,(err) => {
+                    if(err){
+                        return console.log(chalk.red(err))
+                    }
+                    return console.log(chalk.green(`File generated and stored at ${outputDir}`));
+                });
+            }
         }
         else {
             console.log("Invalid bankCode");
         }
     },
-    bankCodes : {
-        list : () => {
+    bankCodes: {
+        list: () => {
             console.log("===========================");
             console.log("Bank               Codes")
             console.log("===========================");
             nubanAlgo.codes.forEach(code => {
-                console.log(chalk.green(code.bank),chalk.yellowBright(code.code));
+                console.log(chalk.green(code.bank), chalk.yellowBright(code.code));
             })
             console.log("===========================");
-            
+
         }
     }
 
--- bin/index.js
+++ bin/index.js
@@ -22,9 +22,13 @@ program.command("generate")
     //.option('-r, --random' ,"Generates the nuban randomly. If not specified, NUBAN generatio will start from 0000000000")  #todo random
     .option('-s, --startFrom [startFrom]','Where to start generating from')
     .option('-l, --limit [limit]', 'Limit of list. Default is 100,000')
+    .option('-sv, --save',"Save to file. works with -o --outputDir")
+    .option('-o, --outputDir <outputDir>', '.json output dir')
+    .option('-p, --print',"Choose to print results or not")
     .action(Commands.generate);
 
 program.command("list-bank-codes")
     .description("list all bank codes")
     .action(Commands.bankCodes.list);
-program.parse()
+program.parse();
+//sterlingaccountapi.appsuport.p.azurewebsites.net/api/User/FioranoGetAccountFullInfo?accountNumber=0068957855
